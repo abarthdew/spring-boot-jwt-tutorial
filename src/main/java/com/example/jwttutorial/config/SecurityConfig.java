c246abed590534cb95e04b00557af82d79abeef9
ABarthDew <vsevolod117@gmail.com>
2023-03-15 22:06:57 +0900

    2-5.security

--- src/main/java/com/example/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.example.jwttutorial.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // h2-console 하위 모든 요청, 파비콘 관련 요청
+    // : security 로직 수행을 하지 않고 접근 가능하게 하는 로직
+    @Override
+    public void configure(WebSecurity web) {
+        web
+            .ignoring()
+            .antMatchers(
+              "/h2-console/**"
+              ,"/favicon.ico"  
+            );
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
